f2125915fde2a383524e6990ba14df9194ef6419
Alaettin Serhan Mete <serhanmete@gmail.com>
2020-06-15 16:49:29 +0200

    Having fun w/ GitHub actions

---
+++ entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/sh -l
+set -e
+
+#cd /tmp
+
+## Setup
+#if [ -z "$CXX" ]; then
+#    CXX=$(type -p g++)
+#fi
+#if [ -z "$CC" ]; then
+#	CC=$(type -p gcc)
+#fi
+#if [ -z "$CMAKE" ]; then
+#	CMAKE=$(type -p cmake)
+#fi
+
+## Configure 
+#cmd="$CMAKE -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_COMPILER=$CC $CMAKE_EXTRA $GITHUB_WORKSPACE"
+#echo $cmd
+#$cmd
+
+## Run clang-format
+#make clang-format
+cd "$GITHUB_WORKSPACE"
+clang-format -style=Google -i src/hello_world.cxx
+
+# Go to the directory
+#cd "$GITHUB_WORKSPACE"
+
+# Configure the author
+echo ">> Configuring the author"
+git config --global user.email "clang-format@github-actions"
+git config --global user.name "clang-format"
+
+# Commit the changes
+echo ">> Committing the changes"
+git commit -a -m "Apply clang-format" || true
+
+# Push to the branch
+echo ">> Pushing to $BRANCH"
+git push -u origin $BRANCH
